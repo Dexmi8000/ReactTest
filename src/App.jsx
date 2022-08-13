591755ff6350c1468aef2ba6914f93832e4366f9
Dexmi8000 <sterchi@mail.ru>
2022-08-13 20:05:34 +0300

    добавил Линки

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Nav from './components/Navigation/Navigation';
 import Main from './components/Main/Main';
 import Messages from './components/Messages/Messages';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes, BrowserRouter } from 'react-router-dom';
 
 
 
@@ -11,26 +11,27 @@ import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    
+    <BrowserRouter>
       <div className="App">
         <div className="dexmi">
           <div className='wrap'>
             <Header />
             <Nav />
             <div className="content-wrapper">
-              
-                <Routes>
-                  <Route path="/Main" element={<Main />} />
-                  <Route path="/Messages" element={<Messages />} />
-                </Routes>
-              
+
+              <Routes>
+                <Route path="/" element={<null />} />
+                <Route path="/Main" element={<Main />} />
+                <Route path="/Messages" element={<Messages />} />
+              </Routes>
+
 
             </div>
 
           </div>
         </div>
       </div>
-    
+    </BrowserRouter>
 
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,6 +2,7 @@ import dexstyle from './Navigation.module.css'
 import icon_message from './icon_message.png'
 import icon_news from './icon_news.png'
 import icon_profile from './icon_profile.png'
+import {Link} from 'react-router-dom';
 
 function Nav() {
     return (
@@ -9,11 +10,11 @@ function Nav() {
             <ul className={dexstyle.nav__items}>
                 <li className={dexstyle.nav__item}>
                     <img className={dexstyle.items__img} src={icon_profile} alt="" />
-                    <a className={dexstyle.nav__link} href="/Main">Profile</a>
+                    <Link className={dexstyle.nav__link} to="/Main">Main</Link>
                 </li>
                 <li className={`${dexstyle.nav__item} ${dexstyle.active}`} >
                     <img className={dexstyle.items__img} src={icon_message} alt="" />
-                    <a className={`${dexstyle.nav__link} ${dexstyle.active}`} href="/Messages">Messages</a>
+                    <Link className={`${dexstyle.nav__link} ${dexstyle.active}`} to="/Messages">Messages</Link>
                 </li>
                 <li className={dexstyle.nav__item}>
                     <img className={dexstyle.items__img} src={icon_news} alt="" />
